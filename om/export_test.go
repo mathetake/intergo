5774eca5e951d3559f0b8a7e0b1e2d754556fe95
mathetake <cz.rk.t0415y.g@gmail.com>
2018-08-10 14:02:20 +0900

    finish implementation of optimized multileaving by insensitivity

--- om/export_test.go
+++ om/export_test.go
@@ -2,7 +2,6 @@ package om
 
 import (
 	"github.com/mathetake/intergo"
-	"gonum.org/v1/gonum/mat"
 )
 
 func (o *OptimizedMultiLeaving) ExportedPrefixConstraintSampling(num int, rks ...intergo.Ranking) []intergo.Res {
@@ -11,8 +10,4 @@ func (o *OptimizedMultiLeaving) ExportedPrefixConstraintSampling(num int, rks ..
 
 func (o *OptimizedMultiLeaving) ExportedCalcInsensitivity(rks []intergo.Ranking, cRks [][]intergo.Res) []float64 {
 	return o.calcInsensitivity(rks, cRks)
-}
-
-func (o *OptimizedMultiLeaving) ExportedGetConstraintMatrix(rks []intergo.Ranking, cRks [][]intergo.Res) *mat.Dense {
-	return o.getConstraintMatrix(rks, cRks)
 }
--- om/om_test.go
+++ om/om_test.go
@@ -25,23 +25,54 @@ var _ intergo.Ranking = tRanking{}
 
 func TestGetInterleavedRanking(t *testing.T) {
 	o := &om.OptimizedMultiLeaving{}
+
 	cases := []struct {
-		inputRks []intergo.Ranking
-		num      int
+		num           int
+		inputRankings []intergo.Ranking
+		expected      []intergo.Res
 	}{
 		{
-			inputRks: []intergo.Ranking{
+			inputRankings: []intergo.Ranking{
 				tRanking{1, 2, 3, 4, 5},
 				tRanking{10, 20, 30, 40, 50},
 			},
+			expected: []intergo.Res{
+				{RankingIDx: 0, ItemIDx: 0},
+				{RankingIDx: 1, ItemIDx: 0},
+			},
+			num: 2,
+		},
+		{
+			inputRankings: []intergo.Ranking{
+				tRanking{1, 2, 3},
+				tRanking{10, 20, 30},
+			},
+			expected: []intergo.Res{
+				{RankingIDx: 0, ItemIDx: 0},
+				{RankingIDx: 1, ItemIDx: 0},
+				{RankingIDx: 1, ItemIDx: 1},
+			},
 			num: 10,
+		},
+		{
+			inputRankings: []intergo.Ranking{
+				tRanking{1, 2, 3, 10, 10, 30},
+				tRanking{10, 20, 30},
+				tRanking{100, 200, 300},
+			},
+			expected: []intergo.Res{
+				{RankingIDx: 0, ItemIDx: 0},
+				{RankingIDx: 1, ItemIDx: 0},
+				{RankingIDx: 2, ItemIDx: 0},
+			},
+			num: 2,
 		},
 	}
 
 	for n, tc := range cases {
 		tcc := tc
 		t.Run(fmt.Sprintf("%d-th unit test", n), func(t *testing.T) {
-			actual, err := o.GetInterleavedRanking(tcc.num, tcc.inputRks...)
+			actual, err := o.GetInterleavedRanking(tcc.num, tcc.inputRankings...)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -272,126 +303,6 @@ func TestCalcInsensitivity(t *testing.T) {
 				}
 				if true != (diff < tcc.threshold) {
 					t.Logf("unexpected difference at %d-th element: actual:%f != expected:%f", i, actual[i], tcc.expected[i])
-				}
-				assert.Equal(t, true, diff < tcc.threshold)
-			}
-		})
-	}
-}
-
-func TestGetConstraintMatrix(t *testing.T) {
-	o := &om.OptimizedMultiLeaving{}
-
-	cases := []struct {
-		inputRankings    []intergo.Ranking
-		combinedRankings [][]intergo.Res
-		expected         []float64
-		threshold        float64
-	}{
-		{
-			inputRankings: []intergo.Ranking{
-				tRanking{1, 2, 3},
-				tRanking{10, 20, 30},
-			},
-			combinedRankings: [][]intergo.Res{
-				{
-					intergo.Res{RankingIDx: 0, ItemIDx: 0},
-					intergo.Res{RankingIDx: 1, ItemIDx: 0},
-				},
-				{
-					intergo.Res{RankingIDx: 0, ItemIDx: 0},
-					intergo.Res{RankingIDx: 0, ItemIDx: 1},
-				},
-			},
-			expected: []float64{
-				1.0, 1.0,
-				0.75, 0.75,
-				-0.75, -0.75,
-				0.625, 0.875,
-				-0.625, -0.875,
-			},
-			threshold: 10e-8,
-		},
-		{
-			inputRankings: []intergo.Ranking{
-				tRanking{1, 2, 3, 4},
-				tRanking{10, 20, 30, 40},
-			},
-			combinedRankings: [][]intergo.Res{
-				{
-					intergo.Res{RankingIDx: 0, ItemIDx: 0},
-					intergo.Res{RankingIDx: 1, ItemIDx: 0},
-					intergo.Res{RankingIDx: 0, ItemIDx: 1},
-				},
-				{
-					intergo.Res{RankingIDx: 0, ItemIDx: 0},
-					intergo.Res{RankingIDx: 0, ItemIDx: 1},
-					intergo.Res{RankingIDx: 1, ItemIDx: 0},
-				},
-			},
-			expected: []float64{
-				1.0, 1.0,
-				0.8, 0.8,
-				-0.8, -0.8,
-				0.65, 0.95,
-				-0.65, -0.95,
-				0.6944444444444444, 0.9055555555555554,
-				-0.6944444444444444, -0.9055555555555554,
-			},
-			threshold: 10e-6,
-		},
-		{
-			inputRankings: []intergo.Ranking{
-				tRanking{1, 2, 3, 4},
-				tRanking{10, 20, 30, 40},
-				tRanking{100, 200, 300, 400},
-			},
-			combinedRankings: [][]intergo.Res{
-				{
-					intergo.Res{RankingIDx: 0, ItemIDx: 0},
-					intergo.Res{RankingIDx: 0, ItemIDx: 1},
-					intergo.Res{RankingIDx: 1, ItemIDx: 0},
-				},
-				{
-					intergo.Res{RankingIDx: 2, ItemIDx: 0},
-					intergo.Res{RankingIDx: 0, ItemIDx: 0},
-					intergo.Res{RankingIDx: 1, ItemIDx: 0},
-				},
-			},
-			expected: []float64{
-				1.0, 1.0,
-				0.8, 0.0,
-				0.0, -0.8,
-				-0.8, 0.8,
-				0.95, 0.15,
-				0.0, -0.8,
-				-0.95, 0.65,
-				0.9055555555555554, 0.10555555555555557,
-				0.04444444444444444, -0.7555555555555555,
-				-0.95, 0.65,
-			},
-			threshold: 10e-6,
-		},
-	}
-	for n, tc := range cases {
-		tcc := tc
-		t.Run(fmt.Sprintf("%d-th unit test", n), func(t *testing.T) {
-			actual := o.ExportedGetConstraintMatrix(tcc.inputRankings, tcc.combinedRankings)
-
-			// verify the dimension of the matrix
-			var ar, ac = actual.Dims()
-			assert.Equal(t, 1+len(tcc.inputRankings)*len(tcc.combinedRankings[0]), ar)
-			assert.Equal(t, len(tcc.combinedRankings), ac)
-			assert.Equal(t, len(tcc.expected), len(actual.RawMatrix().Data))
-
-			// check the values in the matrix
-			for i, v := range actual.RawMatrix().Data {
-				diff := v - tcc.expected[i]
-				if diff < 0 {
-					diff = -diff
-				}
-				if true != (diff < tcc.threshold) {
-					t.Logf("unexpected difference at %d-th element: actual:%f != expected:%f", i, v, tcc.expected[i])
 				}
 				assert.Equal(t, true, diff < tcc.threshold)
 			}
